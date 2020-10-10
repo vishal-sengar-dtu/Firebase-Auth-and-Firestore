5738f2cf7c4730b1082157f67a7078c596177277
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-10 17:31:08 +0530

    Logging Users In

--- auth.js
+++ auth.js
@@ -6,14 +6,12 @@ signupForm.addEventListener("submit", (e) => {
   //get user info
   const email = signupForm["signup-email"].value;
   const password = signupForm["signup-password"].value;
-  console.log(email);
-  console.log(password);
 
   //sign up the user
   auth.createUserWithEmailAndPassword(email, password)
     .then(cred => {
 
-      // clear input fields and close modal 
+      // clear input fields and close signup modal 
       const modal = document.querySelector("#modal-signup")
       M.Modal.getInstance(modal).close();
       signupForm.reset();
@@ -29,4 +27,25 @@ logout.addEventListener("click", (e) => {
   auth.signOut().then(() => {
     alert("user signed out")
   })
+})
+
+//login
+const loginForm = document.querySelector("#login-form")
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  //sign in the user
+  auth.signInWithEmailAndPassword(email, password)
+    .then(cred => {
+      console.log(cred.user);
+
+      // clear input fields and close login modal 
+      const modal = document.querySelector("#modal-login")
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    });
 })
